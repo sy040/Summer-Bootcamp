f44015924553bf0e8d05aa7c4f1e175ab335d2b3
sy040 <shreyaaa2004@gmail.com>
2026-05-28 15:54:58 +0530

    fixed code

--- prefixarray.c
+++ prefixarray.c
@@ -1,21 +1,25 @@
 #include<stdio.h>
 int main(){
 	int n,i,sum,l,r;
+	printf("Enter range:");
 	scanf("%d",&n);
-	int arr[50];
-	int prefix[50];
+	int arr[n];
+	int prefix[n];
 	//indexing of array is from 0 to n
+	printf("Enter array values:");
 	for(i=0;i<n;i++){
 		scanf("%d",&arr[i]);
 	}
+	printf("Enter array range sum start:");
 	scanf("%d",&l);
+	printf("Enter array range sum end:");
 	scanf("%d",&r);
-	int p[50]={0};
+	prefix[n]=0;
 	//indexing 1 to n
 	for(i=1;i<=n;i++){
 		prefix[i]=prefix[i-1]+arr[i-1];
 	}
 	sum=prefix[r]-prefix[l-1];
-	printf("%d",sum);
+	printf("Sum: %d",sum);
 
 }
