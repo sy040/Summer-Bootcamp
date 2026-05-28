72bf3e7fa31bc4b83ca421c1a816232e609fa701
sy040 <shreyaaa2004@gmail.com>
2026-05-28 23:18:48 +0530

    fixed

--- vowel.c
+++ vowel.c
@@ -7,7 +7,7 @@ int main(){
     char ch[100];
     printf("Enter string:");
     fgets(ch,sizeof(ch),stdin);
-    for(i=0;i<sizeof(ch);i++){
+    for(i=0;ch[i]!='\0';i++){
     if(ch[i]=='a'||ch[i]=='e'||ch[i]=='i'||ch[i]=='o'||ch[i]=='u'||ch[i]=='A'||ch[i]=='E'||ch[i]=='I'||ch[i]=='O'||ch[i]=='U'){
         f++;
     }
