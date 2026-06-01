eea4de9ec0b18dd415d28730adaabb259d1116a3
sy040 <shreyaaa2004@gmail.com>
2026-06-02 00:08:53 +0530

    initial commit

---
+++ Day4/lrrotation.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int main() {
+    char str[100], newstr[100];
+    int n, i;
+
+    printf("Enter string: ");
+    scanf("%99s", str);
+
+    printf("Enter rotation: ");
+    scanf("%d", &n);
+
+    char d;
+    printf("Enter direction (L/R): ");
+    scanf(" %c", &d);
+
+    int len = strlen(str);
+
+    n = n % len;
+
+    if (d == 'L' || d == 'l') {
+        for (i = 0; i < len; i++) {
+            newstr[i] = str[(i + n) % len];
+        }
+    }
+    else if (d == 'R' || d == 'r') {
+        for (i = 0; i < len; i++) {
+            newstr[i] = str[(i - n + len) % len];
+        }
+    }
+    
+    newstr[len] = '\0';
+
+    for (i = 0; i < n && i < len; i++) {
+    if (islower(newstr[i]))
+        newstr[i] = toupper(newstr[i]);
+    else if (isupper(newstr[i]))
+        newstr[i] = tolower(newstr[i]);
+}
+
+    printf("Result: %s\n", newstr);
+
+    return 0;
+}
